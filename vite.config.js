ebebc2cbc237a383768862ad28180d9a950bd8ff
Chrissem98 <62968995+Chrissem98@users.noreply.github.com>
2022-09-27 04:48:42 +0000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,90 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'My Vue App',
+        short_name: 'VueApp',
+        description: 'My Awesome App description',
+        start_url: "/?source=pwa",
+        scope: "/",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: '#121212',
+        categories: [
+          "test",
+        ],
+        icons: [
+          {
+            src: 'src/assets/vue-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'src/assets/vue-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        shortcuts: [
+          {
+            "name": "Home",
+            "short_name": "Home",
+            "description": "My Home Page",
+            "url": "/",
+            "icons": [
+              {
+                "src": "/src/assets/home-192x192.png",
+                "sizes": "192x192"
+              }
+            ]
+          },
+          {
+            "name": "About",
+            "short_name": "About",
+            "description": "My About Page",
+            "url": "/about",
+            "icons": [
+              {
+                "src": "/src/assets/about-192x192.png",
+                "sizes": "192x192"
+              }
+            ]
+          },
+        ],
+        screenshots: [
+          {
+            "src": "src/assets/vue-home.png",
+            "sizes": "400x865",
+            "type": "image/png"
+          },
+          {
+            "src": "src/assets/vue-about.png",
+            "sizes": "400x865",
+            "type": "image/png"
+          },
+        ],
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
